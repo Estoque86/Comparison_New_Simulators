4bcee053bef4a186298009ca0da0c5932d06f9ef
Estoque86 <tortellimichele@gmail.com>
2015-09-01 14:55:28 +0200

    single cache

---
+++ ccnpl-sim-0.1.0/single_cache/launch_sim_single_cache_1e4_1e6_1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_1 > stderr_single_cache_1e4_1e6_1_1.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_2 > stderr_single_cache_1e4_1e6_1_2.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_3 > stderr_single_cache_1e4_1e6_1_3.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_4 > stderr_single_cache_1e4_1e6_1_4.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_5 > stderr_single_cache_1e4_1e6_1_5.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_6 > stderr_single_cache_1e4_1e6_1_6.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_7 > stderr_single_cache_1e4_1e6_1_7.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_8 > stderr_single_cache_1e4_1e6_1_8.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_9 > stderr_single_cache_1e4_1e6_1_9.log
+../ccnpl-sim/bin/cbcbsim -input option_file_single_cache_1e4_1e6_1_10 > stderr_single_cache_1e4_1e6_1_10.log
