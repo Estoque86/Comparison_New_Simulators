a7dcda841b357c1a887d209c1c713355bb446a47
Estoque86 <tortellimichele@gmail.com>
2015-09-01 15:29:38 +0200

    single cache

--- ccnpl-sim-0.1.0/single_cache/launch_sim_single_cache_1e4_1e7_1.sh
+++ ccnpl-sim-0.1.0/single_cache/launch_sim_single_cache_1e4_1e7_1.sh
@@ -9,6 +9,7 @@ infoDir=infoSim/R_1e7
 optionDir=option_files/R_1e7
 
 T="SINGLE_CACHE"
+sim="CCNPL"
 C=0.01
 M=10000
 req=10000000
@@ -20,5 +21,5 @@ runs=0
 
 for i in `seq 0 $runs`
 do
-   /usr/bin/time -f "\n%E \t elapsed real time \n%U \t total CPU seconds used (user mode) \n%S \t total CPU seconds used by the system on behalf of the process \n%M \t memory (max resident set size) [kBytes] \n%x \t exit status"  -o ${infoDir}/R_1e7/Info_SIM\=${sim}_T\=${T}_REQ\=${req}_M\=${M}_C\=${C}_L\=${lam}_A\=${alpha}_R\=${i}.txt  ../ccnpl-sim/bin/cbcbsim -input ${optionDir}/option_file_single_cache_1e4_1e7_${alpha}_${i} > stderr_single_cache_1e4_1e7_${alpha}_${i}.log 2>&1
+   /usr/bin/time -f "\n%E \t elapsed real time \n%U \t total CPU seconds used (user mode) \n%S \t total CPU seconds used by the system on behalf of the process \n%M \t memory (max resident set size) [kBytes] \n%x \t exit status"  -o ${infoDir}/Info_SIM\=${sim}_T\=${T}_REQ\=${req}_M\=${M}_C\=${C}_L\=${lam}_A\=${alpha}_R\=${i}.txt  ../ccnpl-sim/bin/cbcbsim -input ${optionDir}/option_file_single_cache_1e4_1e7_${alpha}_${i} > stderr_single_cache_1e4_1e7_${alpha}_${i}.log 2>&1
 done
